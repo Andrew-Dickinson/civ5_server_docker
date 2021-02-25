44c025b3e5b5e75ddc8be10cc3a14c97a5a4ead5
CraftedCart <CraftedCart@outlook.com>
2021-02-25 18:29:09 +0000

    Don't need to forward the steam networking port

--- run.sh
+++ run.sh
@@ -8,7 +8,6 @@ DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 #### Ports ####
 # 5900 TCP: VNC
-# 27015 UDP: Dedicated server gameplay traffic via Steam
 # 27016 UDP: Civ 5 pitboss
 
 # We only accept VNC connections from `127.0.0.1` - to access it, set up an ssh tunnel with
@@ -20,6 +19,5 @@ docker run \
     --mount type=bind,source="$DIR/civ5game",target="/root/civ5game" \
     --mount type=bind,source="$DIR/civ5save",target="/root/My Games/Sid Meier's Civilization 5" \
     -p 127.0.0.1:5900:5900/tcp \
-    -p 27015:27015/udp \
     -p 27016:27016/udp \
     civ5server
