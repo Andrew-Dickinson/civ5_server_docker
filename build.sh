69418352b443b8f580931eba40fe9657bcf8cfaf
Andrew Dickinson <andrew.dickinson.0216@gmail.com>
2021-09-06 04:11:40 +0000

    Add customized game files

--- build.sh
+++ build.sh
@@ -6,5 +6,8 @@ set -e
 # Fetch the dir where this bash script is
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+sudo cp "$DIR/server/MPList.lua" "$DIR/civ5game/Assets/UI/InGame/WorldView/MPList.lua"
+sudo cp "$DIR/server/UserSettings.ini" "$DIR/civ5save/UserSettings.ini"
+
 # Build the container
 docker build -t civ5server "$DIR/server"
